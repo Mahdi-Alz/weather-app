5dd5ac07bea55b36d9efb2d17a491b6200efa55d
Mahdi-Alz <mahdialz.dev@gmail.com>
2026-06-23 10:12:19 +0330

    project init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Weather Application</h1>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// fonts
+import "@fontsource/inter";
+import "@fontsource/roboto";
+import "@fontsource/poppins";
+
+createApp(App).mount("#app");
