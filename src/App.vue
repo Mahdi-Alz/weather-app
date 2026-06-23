dbc10e1fbcc861f8ad8387901ba28842826331b4
Mahdi-Alz <mahdialz.dev@gmail.com>
2026-06-23 12:23:20 +0330

    feat: search field and related api calls

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import SearchInput from "./components/SearchInput.vue";
+
+const places = ref([]);
+const addPlace = (data) => {
+  places.value.push(data);
+};
+</script>
 
 <template>
-  <h1>Weather Application</h1>
+  <main class="min-h-dvh bg-zinc-100 p-8">
+    <h1 class="text-center">Weather Application</h1>
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString("en-us", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })
+      }}
+    </div>
+    <!-- Search -->
+    <div>
+      <SearchInput @place-data="addPlace" />
+    </div>
+    <!-- weather cards -->
+    <div>
+      <p v-for="place in places">{{ place.location.name }}</p>
+    </div>
+  </main>
 </template>
-
-<style scoped></style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { reactive } from "vue";
+
+const emit = defineEmits(["place-data"]);
+
+const searchTerm = reactive({
+  query: "",
+  timeout: null,
+  results: null,
+});
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout);
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query.length) {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=5805c27e61e94d369c674729262306&q=${searchTerm.query}`,
+      );
+      const data = await res.json();
+      searchTerm.results = data;
+    } else {
+      searchTerm.results = null;
+    }
+  }, 500);
+};
+
+const getWeather = async (id) => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=5805c27e61e94d369c674729262306&q=id:${id}&days=3&aqi=no&alerts=no`,
+  );
+  const data = await res.json();
+  emit("place-data", data);
+};
+</script>
+
+<template>
+  <div>
+    <!-- search field -->
+    <form>
+      <div
+        class="bg-white border border-indigo-600/30 rounded-lg shadow-lg flex items-center"
+      >
+        <span
+          class="flex items-center justify-center p-2 shrink-0 text-indigo-600"
+        >
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </span>
+
+        <input
+          type="text"
+          placeholder="Search for a place"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
+          v-model="searchTerm.query"
+          @input="handleSearch"
+        />
+      </div>
+    </form>
+    <!-- search suggestions -->
+    <div class="bg-white my-2 rounded-lg shadow-lg">
+      <div v-if="searchTerm.results">
+        <div v-for="place in searchTerm.results" :key="place.id">
+          <button
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold transition-all duration-200 ease-in-out w-full text-left"
+            @click="getWeather(place.id)"
+          >
+            {{ place.name }}, {{ place.region }}, {{ place.country }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
